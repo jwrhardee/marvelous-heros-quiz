bd566a9567611191bb131fa5c40ec91a9dd9b870
jesseHardee <jwrhardee@gmail.com>
2020-08-03 21:29:02 +0000

    answers

--- main.py
+++ main.py
@@ -16,3 +16,15 @@ Heroes = {
     "DBA" : "Hawkeye",
     "DBB" : "Black Widow"
 }
+
+name = input("hello what is your name")
+answer = input ("hi "+ name + " ready to play y or n")
+
+if answer.upper() == "Y" :
+  question1 = input("what color do you prefer? (Choose a or b) \na Blue \nb red")
+if answer.upper() == "Y" :
+  question2 = input ("who do you think is stronger A.thor or A.hulk \nA \nB" ) 
+  if answer.upper() == "Y" :
+    question3 = input ("who is your faviroit Spiderman A.tom B.andrew \nA \nB" )
+    if wait.upper()  == "Y": 
+      print 
